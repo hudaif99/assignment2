6e70ff34e73e8beb9499442259563ec756ad30bc
hudaif99 <hudaifpaladan99@gmail.com>
2021-12-22 21:34:15 +0530

    indian flag

---
+++ android/app/src/main/kotlin/com/coderacademykerala/assignment2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.coderacademykerala.assignment2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: page1(),
+    );
+  }
+}
+
+class page1 extends StatefulWidget {
+  const page1({Key? key}) : super(key: key);
+
+  @override
+  _page1State createState() => _page1State();
+}
+
+class _page1State extends State<page1> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.cyan,
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              color: Colors.orange,
+              width: 150,
+              height: 40,
+            ),
+            Container(
+              color: Colors.white,
+              width: 150,
+              height: 40,
+            ),
+            Container(
+              color: Colors.green,
+              width: 150,
+              height: 40,
+            ),
+          ],
+        ),
+      ),
+    );
+
+  }
+}
+
